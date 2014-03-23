19724d87cac2a44e15011d622e608ebcf0573e92
Vinicius Azevedo <azevedo.vinicius@gmail.com>
2014-03-23 19:16:20 -0300

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Boraweb::Application.config.secret_key_base = '4ecedeebf88390eef4aad8b577dc542c9c6febe2ae2d6dea294d2d118502169cd09a62936888794b1e51daa1f6dcb4abf1a53b29dac13e944cba2fe89006f978'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+Boraweb::Application.config.session_store :cookie_store, key: '_boraweb_session'
