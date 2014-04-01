da5528c496776a65f7231fc691816e88090e53d0
Vinicius Azevedo <azevedo.vinicius@gmail.com>
2014-04-01 15:36:46 -0300

    heroku vs paperclip

--- app/models/idea.rb
+++ app/models/idea.rb
@@ -1,4 +1,10 @@
 class Idea < ActiveRecord::Base
 	belongs_to :user
-	has_attached_file :image, :styles => { :medium => "510x510>", :thumb => "210x210>" }
+	has_attached_file :image, styles: { 
+		medium: "510x510>", 
+		thumb: "210x210>" 
+	}
+
+	# Validate the attached image is image/jpg, image/png, etc
+	validates_attachment_content_type :avatar, :content_type => /\Aimage\/.*\Z/
 end
