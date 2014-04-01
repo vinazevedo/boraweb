8b9d321985706243773627a5cdef975262f57616
Vinicius Azevedo <azevedo.vinicius@gmail.com>
2014-04-01 01:54:38 -0300

    heroku vs paperclip

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -81,7 +81,7 @@ Boraweb::Application.configure do
   #Added this to stop this message '[deprecated] I18n.enforce_available_locales 
   #will default to true in the future. If you really want to skip validation 
   #of your locale you can set I18n.enforce_available_locales = false to avoid this message.'
-  config.i18n.enforce_available_locales = false
+  #config.i18n.enforce_available_locales = false
 
   #For Paperclip to work with Amazon S3
   config.paperclip_defaults = {
