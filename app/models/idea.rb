baf9f06287334fe27b283a4f7de1ec395a3b5c10
Vinicius Azevedo <azevedo.vinicius@gmail.com>
2014-04-01 01:28:23 -0300

    heroku vs paperclip

--- app/models/idea.rb
+++ app/models/idea.rb
@@ -1,6 +1,4 @@
 class Idea < ActiveRecord::Base
 	belongs_to :user
-	has_attached_file :image, :styles => { :medium => "510x510>", :thumb => "210x210>" },
-										:storage => s3,
-										:bucket => ENV['boraweb']
+	has_attached_file :image, :styles => { :medium => "510x510>", :thumb => "210x210>" }
 end
